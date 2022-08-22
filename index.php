34440a5d471527fb3bcc89b8ddd570c45d793b87
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-08-22 13:59:19 +0200

    axios cdn added

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=\, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
 </head>
 <body>
     <div id="root">
